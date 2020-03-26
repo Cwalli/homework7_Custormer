65c5360a1b845c2b3077f9cd575f9c59e198f6f5
HENADZI SURAVITSKI <cwall@mail.ru>
2020-03-26 12:47:27 +0300

    homework7+

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Customer Customer1 = new Customer("Иван","Иванов","+777374274277");
-//        Address Address1 = new Address("Минск", "Ивановская", "7");
-
         Customer Customer2 = new Customer("Петр","Петров","+7773745343534");
-//        Address Address2 = new Address("Минск", "Петровская", "7");
         Customer Customer3 = new Customer("Поп","Попов","+7773555555555");
-//        Address Address3 = new Address("Минск", "Поповская", "7");
         Customer Customer4 = new Customer("Поп","Попов","+7773555555555");
 
         // Создание коллекции типа List и вывод ее в консоль
@@ -21,6 +17,7 @@ public class Main {
         customers.add(Customer2.toString());
         customers.add(Customer3.toString());
         customers.add(Customer4.toString());
+        System.out.println("-------------------------------------------------");
         System.out.println("Создание коллекции типа List и вывод ее в консоль");
         for (String custom : customers) {
             System.out.println(custom);
@@ -32,6 +29,7 @@ public class Main {
         customersSet.add(Customer2.toString());
         customersSet.add(Customer3.toString());
         customersSet.add(Customer4.toString());
+        System.out.println("-------------------------------------------------");
         System.out.println("Создание коллекции типа Set и вывод ее в консоль");
         for (String  item : customersSet) {
             System.out.println(item);
@@ -42,17 +40,19 @@ public class Main {
         vocabulary.put("аббат", "Настоятель мужского католического монастыря.");
         vocabulary.put("абзац", "Красная строка, отступ в начале строки.");
         vocabulary.put("абонемент", "Документ, предоставляющий право на пользование чем-н., какое-н. обслуживание, а также само такое право.");
+        System.out.println("-------------------------------------------------");
         System.out.println("Создание коллекции типа Map и вывод ее в консоль");
         for(Map.Entry<String, String > item : vocabulary.entrySet()){
 
             System.out.printf("%s - %s \n", item.getKey(), item.getValue());
         }
+        System.out.println("-------------------------------------------------");
         System.out.println("Толковый словарь НЕдаля");
 
         for( int i=0 ; i!=4;) {
             Scanner in = new Scanner(System.in);
             Scanner inWord = new Scanner(System.in);
-            Scanner WriteNewBuilding = new Scanner(System.in);
+            System.out.println("-------------------------------------------------");
             System.out.println("Введите номер необходимого действия:" + "\n" + "1 - Вывод значения по введенному слову"
                     + "\n" + "2 - Редактирование существующего значения слова" + "\n" + "3 - Добавление нового слова"
                     + "\n" + "4 - Выход из программы");
